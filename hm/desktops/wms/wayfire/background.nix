27e5e50bf6ead4a4f4b2c6525d6aea312c420633
Johannes May <johannes@may-se.de>
2022-05-26 17:36:34 +0200

    Switch to swaybg with a small randomizer script for backgrounds

--- hm/desktops/wms/wayfire/background.nix
+++ hm/desktops/wms/wayfire/background.nix
@@ -1,18 +1,30 @@
 {pkgs, lib, ...}:
 
 {
-  xdg.configFile."wf-shell.ini".text = lib.generators.toINI {} {
-    background = {
-      image = "/home/may/Pictures/wallpapers"; # Wallpaper file or directory
-      preserve_aspect = 1; # Keep aspect ratio on images that don't match the displays resolution
-      cycle_timeout = 150; # Wallpaper changing timeout in seconds
-      randomize = 1; # Randomize wallpaper order
-    };
-  };
-
-  systemd.user.services.wfbackground = {
+  systemd.user.services.wayfire_background = let swaybg_randomize = (
+    pkgs.writeTextFile {
+      name = "swaybg_randomize";
+      destination = "/bin/swaybg_randomize";
+      executable = true;
+      text = ''
+        #! ${pkgs.bash}/bin/bash
+        
+        WALLPAPER_PATH="$HOME/Pictures/wallpapers"
+        # Only checking for extensions here, should do some kind of mimetype check as well.
+        
+        RANDOM_WALLPAPER=$(find "$WALLPAPER_PATH" -type f \( -name "*.jpg" -o -name "*.png" \) | shuf -n 1)
+        if [ -n "$RANDOM_WALLPAPER" ]; then
+          ${pkgs.swaybg}/bin/swaybg --mode fill --image "$RANDOM_WALLPAPER"
+        else
+          >&2 echo "No wallpapers found"
+        fi
+      '';
+    }
+    );
+  in
+  {  
     Unit = {
-      Description = "Wayfire background image";
+      Description = "Wayfire background image through swaybg";
       Requires = [ "graphical-session-pre.target" ];
       After = [ "graphical-session-pre.target" "tray.target" ];
       PartOf = [ "graphical-session.target" ];
@@ -21,7 +33,7 @@
     Install = { WantedBy = [ "graphical-session.target" ]; };
 
     Service = {
-      ExecStart = "${pkgs.wayfirePlugins.wf-shell}/bin/wf-background";
+      ExecStart = "${swaybg_randomize}/bin/swaybg_randomize";
     };
   };
 }
