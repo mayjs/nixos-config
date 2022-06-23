bc4d59ad952cc1d99c5532353f94b53346eb9ad6
Johannes May <johannes@may-se.de>
2022-06-23 22:33:30 +0200

    Switch console mode for the bootloader to get higher resolution output

--- modules/misc/bootloader.nix
+++ modules/misc/bootloader.nix
@@ -3,5 +3,6 @@
 {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.consoleMode = "auto";
   boot.loader.efi.canTouchEfiVariables = true;
 }
