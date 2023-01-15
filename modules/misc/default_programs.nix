61b89f02506c2851fa5bd045bd1b8ae199ed8c7a
Johannes May <johannes@may-se.de>
2023-01-15 22:08:00 +0100

    Update to latest nixpkgs unstable

--- modules/misc/default_programs.nix
+++ modules/misc/default_programs.nix
@@ -3,7 +3,8 @@
 {
   environment.systemPackages = with pkgs; [
      wget neovim htop git bash
-     ecryptfs ecryptfs-helper
+     ecryptfs
+     # ecryptfs-helper # FIXME: Broken as of 23/01/15 due to Python 2.7 dependency
      borgbackup
   ];
 }
