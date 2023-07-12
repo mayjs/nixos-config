fc82dd0dc449da8923de8daa34285d70b35b2b97
Johannes May <johannes@may-se.de>
2023-07-12 21:27:11 +0200

    Enable gnome and virt manager

---
+++ modules/gnome.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  programs.dconf.enable = true;
+}
